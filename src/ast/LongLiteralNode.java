04bdf2b8bde434186957eec5a83b4e921fc23e61
mohamad <madsams024@gmail.com>
2019-07-06 16:31:51 +0430

    Long literal added

---
+++ src/ast/LongLiteralNode.java
@@ -0,0 +1,19 @@
+package ast;
+
+public class LongLiteralNode extends BaseASTNode {
+    private long value;
+
+    public LongLiteralNode(long value) {
+        super(NodeType.LONG_LITERAL);
+        this.value = value;
+    }
+
+    public long getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return getNodeType() + " (" + value + ")";
+    }
+}
