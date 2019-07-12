c4690adcbce2f7acf35768dfcb4ab43492dd5c5f
Mohamad Seyfayi <madsams024@gmail.com>
2019-07-13 01:45:21 +0430

    Signature of primitive type fixed

--- src/ast/PrimitiveType.java
+++ src/ast/PrimitiveType.java
@@ -4,19 +4,19 @@ package ast;
  * An enum that shows type of a var
  */
 public enum PrimitiveType {
-    BOOL ("Z"),
-    CHAR ("C"),
-    DOUBLE ("D"),
-    INT ("I"),
-    VOID ("V"),
-    LONG(""),
+    BOOL ("i8"),
+    CHAR ("i8"),
+    INT ("i32"),
+    LONG("i64"),
+    DOUBLE ("double"),
+    FLOAT ("float"),
+    VOID ("void"),
+    //todo
     STRING(""),
-    AUTO(""),
-    FLOAT (" ");
+    AUTO("");
 
-    /*todo need to change to llvm syntax*/
     private final String signature;
-    
+
     PrimitiveType(String signature) {
         this.signature = signature;
     }
