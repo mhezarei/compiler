650b71c9441600b3dba47eb39782fc73f4c19c84
Mohamad Seyfayi <madsams024@gmail.com>
2019-07-14 05:04:17 +0430

    Method access bug 2 fixed

--- src/codegen/Signature.java
+++ src/codegen/Signature.java
@@ -1,53 +1,54 @@
 package codegen;
 
+import ast.PrimitiveType;
 import ast.TypeNode;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Signature {
-    static class Argument{
-        private TypeNode type;
-        private String name;
+class Argument {
+    private PrimitiveType type;
+    private String name;
 
-        public Argument(TypeNode type, String name) {
-            this.type = type;
-            this.name = name;
-        }
-
-        public TypeNode getType() {
-            return type;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Argument argument = (Argument) o;
-            return Objects.equals(name, argument.name);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(type, name);
-        }
+    Argument(PrimitiveType type, String name) {
+        this.type = type;
+        this.name = name;
     }
 
+    public PrimitiveType getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Argument argument = (Argument) o;
+        return Objects.equals(name, argument.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, name);
+    }
+}
+
+public class Signature {
     private TypeNode returnType;
     private String name;
-    private List<Argument> args=new ArrayList<>();
+    private List<Argument> args = new ArrayList<>();
 
     Signature(TypeNode returnType, String name) {
         this.returnType = returnType;
         this.name = name;
     }
 
-    public void addArgs(List<Argument> arguments) {
+    void addArgs(List<Argument> arguments) {
         args.addAll(arguments);
     }
 
@@ -63,19 +64,26 @@ public class Signature {
         return args;
     }
 
+    private boolean checkArguments(Signature signature) {
+        Argument[] args1 = args.toArray(new Argument[0]);
+        Argument[] args2 = signature.args.toArray(new Argument[0]);
+        if(args1.length!=args2.length)
+            return false;
+        for (int i = 0; i < args1.length; i++)
+            if(args1[i].getType() != args2[i].getType())
+                return false;
+
+        return true;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Signature signature = (Signature) o;
-        boolean argTypeCheck=true;
-        Argument[] args1=args.toArray(new Argument[0]);
-        Argument[] args2=signature.args.toArray(new Argument[0]);
-        for (int i = 0; i < args1.length; i++)
-            argTypeCheck = args1[i] == args2[i];
-        return returnType == signature.returnType &&
-                Objects.equals(name, signature.name) &&
-                argTypeCheck;
+
+        return Objects.equals(name, signature.name) &&
+                checkArguments(signature);
     }
 
     @Override
